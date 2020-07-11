025d66a6084c3624e0188292bce525ed34f32013
Mike Esposito <mikeesposito6852@gmail.com>
2020-07-11 08:31:52 -0700

    Reformat comments as task list

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 class Node
 
-  # Build a Node class. It should have attributes for the data it stores
+  # [x] Build a Node class. It should have attributes for the data it stores
     # as well as its left and right children.
-  # As a bonus, try including the Comparable module
+  # [x] As a bonus, try including the Comparable module
     # and make nodes compare using their data attribute.
 
   include Comparable
@@ -30,50 +30,52 @@ p node2 <=> node3
 
 class Tree
 
-  # Build a Tree class which accepts an array when initialized. The Tree class should have a
+  # [] Build a Tree class which accepts an array when initialized. The Tree class should have a
     # root attribute which uses the return value of #build_tree which you’ll write next.
 
-  # #build_tree method which takes an array of data (e.g. [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-    #and turns it into a balanced binary tree full of Node objects appropriately placed
-    # (don’t forget to sort and remove duplicates!). The #build_tree method should return the level-1 root node.
+  # [] #build_tree method which takes an array of data
+    # (e.g. [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]) and turns it into a balanced
+    # binary tree full of Node objects appropriately placed (don’t forget to sort and remove
+    # duplicates!). The #build_tree method should return the level-1 root node.
 
-  # #insert and #delete method which accepts a value to insert/delete
+  # [] #insert and #delete method which accepts a value to insert/delete
     # (you’ll have to deal with several cases for delete such as when a node has children or not).
     # If you need additional resources, check out these two articles on inserting and deleting,
     # or this video with several visual examples.
 
-    # #find method which accepts a value and returns the node with the given value.
+  # [] #find method which accepts a value and returns the node with the given value.
 
-    # #level_order method that returns an array of values.
-      # This method should traverse the tree in breadth-first level order.
-      # This method can be implemented using either iteration or recursion (try implementing both!).
-      # Tip: You will want to use an array acting as a queue to keep track of all the child nodes
-      # that you have yet to traverse and to add new ones to the list (as you saw in the video).
+  # [] #level_order method that returns an array of values.
+    # This method should traverse the tree in breadth-first level order.
+    # This method can be implemented using either iteration or recursion (try implementing both!).
+    # Tip: You will want to use an array acting as a queue to keep track of all the child nodes
+    # that you have yet to traverse and to add new ones to the list (as you saw in the video).
 
-    # #inorder, #preorder, and #postorder methods that returns an array of values. Each method should
-      # traverse the tree in their respective depth-first order.
+  # [] #inorder, #preorder, and #postorder methods that returns an array of values. Each method
+     # should traverse the tree in their respective depth-first order.
 
-    # #depth method which accepts a node and returns the depth(number of levels) beneath the node.
+  # [] #depth method which accepts a node and returns the depth(number of levels) beneath the node.
 
-    # #balanced? method which checks if the tree is balanced.
-      # A balanced tree is one where the difference between heights of left subtree and right subtree
-      # of every node is not more than 1.
+  # [] #balanced? method which checks if the tree is balanced.
+    # A balanced tree is one where the difference between heights of left subtree and right subtree
+    # of every node is not more than 1.
 
-    # #rebalance method which rebalances an unbalanced tree.
-      # Tip: You’ll want to create a level-order array of the tree before passing the array back
-      # into the #build_tree method.
+  # [] #rebalance method which rebalances an unbalanced tree.
+    # Tip: You’ll want to create a level-order array of the tree before passing the array back
+    # into the #build_tree method.
 
 =begin
-    Write a simple driver script that does the following:
+  Write a simple driver script that does the following:
 
-    1. Create a binary search tree from an array of random numbers (`Array.new(15) { rand(1..100) }`)
-    2. Confirm that the tree is balanced by calling `#balanced?`
-    3. Print out all elements in level, pre, post, and in order
-    4. try to unbalance the tree by adding several numbers > 100
-    5. Confirm that the tree is unbalanced by calling `#balanced?`
-    6. Balance the tree by calling `#rebalance`
-    7. Confirm that the tree is balanced by calling `#balanced?`
-    8. Print out all elements in level, pre, post, and in order
+  [] 1. Create a binary search tree from an array of random numbers
+        (`Array.new(15) { rand(1..100) }`)
+  [] 2. Confirm that the tree is balanced by calling `#balanced?`
+  [] 3. Print out all elements in level, pre, post, and in order
+  [] 4. try to unbalance the tree by adding several numbers > 100
+  [] 5. Confirm that the tree is unbalanced by calling `#balanced?`
+  [] 6. Balance the tree by calling `#rebalance`
+  [] 7. Confirm that the tree is balanced by calling `#balanced?`
+  [] 8. Print out all elements in level, pre, post, and in order
 =end
 
 end
